88d83e4b0c922643fea4dcc0504324ec0a3f946d
Ucizi Mafeni <ucizim@gmail.com>
2020-04-25 09:45:20 +0100

    core structure

--- src/JsonParser.hs
+++ src/JsonParser.hs
@@ -1,5 +1,34 @@
 -- |
 
 module JsonParser where
+data JsonValue
+  = JsonNull
+  | JsonNumber Int
+  | JsonString String
+  | JsonList [JsonValue]
+  | JsonObject [(String, JsonValue)]
+  deriving (Show, Eq)
 
-data Json = JsonNumber Int | JsonString String | JsonList [Json] | JsonObject [(String, Json)]
+
+newtype Parser a = Parser
+  { runParser :: String -> Maybe (String, a)
+  }
+
+jsonValue :: Parser JsonValue
+jsonValue = undefined
+
+jsonNull :: Parser JsonValue
+jsonNull = undefined
+
+jsonNumber :: Parser JsonValue
+jsonNumber = undefined
+
+jsonString :: Parser JsonValue
+jsonString = undefined
+
+jsonList :: Parser JsonValue
+jsonList = undefined
+
+jsonObject :: Parser JsonValue
+jsonObject = undefined
+
