9ca8b3317b643fa11dad906213cf5f7c4bda403a
Ucizi Mafeni <ucizim@gmail.com>
2020-04-26 23:28:12 +0100

    test functor laws

--- test/JsonParserSpec.hs
+++ test/JsonParserSpec.hs
@@ -2,8 +2,10 @@
 
 module JsonParserSpec where
 
-import JsonParser
-import Test.Hspec
+import           JsonParser
+import           Test.Hspec
+import           Test.QuickCheck
+import           Data.Char
 
 spec :: Spec
 spec = do
@@ -17,3 +19,18 @@ spec = do
         xParser "yx" `shouldBe` Nothing
       it "input string is empty" $
         xParser "" `shouldBe` Nothing
+  describe  "Functor Parser" $ do
+    it "fmap id = id" $
+      property (functorIdProp $ charP 'x')
+    it "fmap (f . g) == fmap f . fmap g" $
+      property (functorComposeProp (charP  'x') (+1) ord)
+
+
+functorIdProp :: (Eq a) => Parser a -> String -> Bool
+functorIdProp parser input =
+  runParser (fmap id parser) input == id (runParser parser input)
+
+functorComposeProp :: (Eq c) => Parser a -> (b -> c) -> (a -> b) -> String -> Bool
+functorComposeProp parser f g input   =
+  runParser (fmap (f . g) parser) input
+    == runParser ((fmap f) . (fmap g) $ parser) input
