dc8e4b5393a9ee1f0c96f9b000228dbfce52ac03
Ucizi Mafeni <ucizim@gmail.com>
2020-05-05 13:36:35 +0100

    main: take json file and input and dump parse

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import JsonParser
+import System.Environment
+
 main :: IO ()
-main = putStrLn "I am Main"
+main = do
+  [fp] <- getArgs
+  json <- parseFile fp
+  putStrLn $ show json
+
+
+parseFile :: FilePath -> IO (Maybe (String, JsonValue))
+parseFile fp = do
+  input <- readFile fp
+  return (runParser jsonValue input)
