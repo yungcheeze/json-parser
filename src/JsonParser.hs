9846e8a9f9194819858444473ea8505cf534779a
Ucizi Mafeni <ucizim@gmail.com>
2020-05-05 13:57:42 +0100

    move parseFile to bottom

--- src/JsonParser.hs
+++ src/JsonParser.hs
@@ -53,13 +53,6 @@ instance Alternative Parser where
   empty = Parser $ const Nothing
   (Parser p1) <|> (Parser p2) = Parser $ \input -> p1 input <|> p2 input
 
-parseFile :: FilePath -> IO (Maybe (String, JsonValue))
-parseFile fp = do
-  input <- readFile fp
-  print input
-  putStrLn ""
-  return (runParser jsonValue input)
-
 jsonValue :: Parser JsonValue
 jsonValue =
   surroundWs
@@ -153,3 +146,10 @@ predP f = Parser parseIfPred
 
 optionalP :: Parser String -> Parser String
 optionalP p = fromMaybe "" <$> optional p
+
+parseFile :: FilePath -> IO (Maybe (String, JsonValue))
+parseFile fp = do
+  input <- readFile fp
+  print input
+  putStrLn ""
+  return (runParser jsonValue input)
